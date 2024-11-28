ffb162fa4a1721654fd637f377d3c93c2a765784
chamaradw <chamaradw@gmail.com>
2024-11-28 16:44:51 +0530

    testing commit

---
+++ models/product.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name : String,
+  price : Number,
+  description : String,
+  lastPrice : Number
+})
+
+const Product = mongoose.model("products",productSchema)
+
+export default Product;
