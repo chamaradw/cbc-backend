922bcb2e7bb4249e695caf31794dd99a79fcbb84
Don Chamara Wanniarachchi <chamaradw@gmail.com>
2025-01-29 23:33:42 +0530

    error corect

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { createProduct, getProducts, updateProduct, deleteProduct, getProductsById} from '../controllers/productController.js';
+import { createProduct, getProducts, updateProduct, deleteProduct, getProductsById, searchProducts} from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProducts)
+productRouter.get("/search/:query",searchProducts)
 productRouter.get("/:productId",getProductsById)
 productRouter.put("/:productId",updateProduct)
 productRouter.delete("/:productId",deleteProduct)
-productRouter.get("/search/:query",searchProducts)
+
 
 export default productRouter; 
