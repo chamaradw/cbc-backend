25ebbb195d5d0ffe71ebc82c63b057381da17687
Don Chamara Wanniarachchi <chamaradw@gmail.com>
2025-02-03 11:12:20 +0530

    updated user schema

--- models/user.js
+++ models/user.js
@@ -1,26 +1,41 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  email : {type : String,required : true,unique : true},
-  firstName : {type : String,required : true},
-  lastName : {type : String,required : true},
-  password : {type : String,required : true},
-  isBlocked : {type : Boolean,default : false},
-  type : {type : String,default : "customer"},
-  profilePicture : {type : String,default : 
-    "https://img.freepik.com/free-vector/user-blue-gradient_78370-4692.jpg?t=st=1731770840~exp=1731774440~hmac=0f8ac074c2321289f63858728e073c0d5946ea6cb542c3b259a88e367a7dde25&w=740"}
-})
-const User = mongoose.model("users",userSchema)
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  password: { type: String, required: true },
+  isBlocked: { type: Boolean, default: false },
+  type: { type: String, default: "customer" },
+  fullName: { type: String, required: true }, // This will be auto-filled
+  address: { type: String, required: true },
+  deliveryAddress: { type: String, required: true },
+  phone1: { type: String, required: true },
+  phone2: { type: String },
+  profilePicture: {
+    type: String,
+    default:
+      "https://img.freepik.com/free-vector/user-blue-gradient_78370-4692.jpg?t=st=1731770840~exp=1731774440~hmac=0f8ac074c2321289f63858728e073c0d5946ea6cb542c3b259a88e367a7dde25&w=740",
+  },
+});
+
+// Pre-save hook to generate full name
+userSchema.pre("save", function (next) {
+  this.fullName = `${this.firstName} ${this.lastName}`.trim();
+  next();
+});
+
+// Pre-update hook to update full name when firstName or lastName changes
+userSchema.pre("findOneAndUpdate", function (next) {
+  const update = this.getUpdate();
+  if (update.firstName || update.lastName) {
+    update.fullName = `${update.firstName || this.firstName} ${update.lastName || this.lastName}`.trim();
+  }
+  next();
+});
+
+const User = mongoose.model("users", userSchema);
 export default User;
-
-
-
-
-
-
-
-
-
 
 
 
