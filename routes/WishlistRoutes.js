29f7269821291a75975a03dcf50b8a4e492849be
Don Chamara Wanniarachchi <chamaradw@gmail.com>
2025-02-16 08:03:39 +0530

    little cahnge

--- routes/WishlistRoutes.js
+++ routes/WishlistRoutes.js
@@ -2,9 +2,9 @@ import { Router } from "express";
 import {addToWishlist,removeFromWishlist,getWishlist,} from "../controllers/wishlistController.js";
 
 const router = Router();
-
+router.get("/:email", getWishlist);
 router.post("/add", addToWishlist);
 router.delete("/remove", removeFromWishlist);
-router.get("/:email", getWishlist);
+
 
 export default router;
